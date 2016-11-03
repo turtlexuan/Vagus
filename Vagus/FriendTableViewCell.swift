c2dda2e03583fd474121cb1b2d881005203a491f
turtlexuan <turtlexuan@cherri.tech>
2016-11-04 01:04:17 +0800

    1. Add FriendVC 2. Add Friend TableViewCell.

---
+++ Vagus/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  Vagus
+//
+//  Created by Mac on 11/4/16.
+//  Copyright © 2016 楊信之. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Vagus/FriendViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FriendViewController.swift
+//  Vagus
+//
+//  Created by Mac on 11/4/16.
+//  Copyright © 2016 楊信之. All rights reserved.
+//
+
+import UIKit
+
+class FriendViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
