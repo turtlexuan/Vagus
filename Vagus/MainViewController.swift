c4f7947e1cd0bd4feb6de0baa73d1b8da8375d24
turtlexuan <turtlexuan@cherri.tech>
2016-11-04 00:41:00 +0800

    1. Main Page VC.

---
+++ Vagus/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  Vagus
+//
+//  Created by Mac on 11/4/16.
+//  Copyright © 2016 楊信之. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var userProfileImage     : UIImageView!
+    @IBOutlet weak var userID               : UILabel!
+    @IBOutlet weak var userAge              : UILabel!
+    @IBOutlet weak var userHobby            : UILabel!
+    @IBOutlet weak var userSpeciality       : UILabel!
+    @IBOutlet weak var userLevel            : UILabel!
+    @IBOutlet weak var levelImage           : UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goChatAction(_ sender: Any) {
+    }
+
+    @IBAction func goFriendManagerAction(_ sender: Any) {
+    }
+
+    @IBAction func nextVC(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
